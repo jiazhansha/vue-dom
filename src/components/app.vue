4d272466f90fe4b94560ec31dbdde9aea31e851a
1065277371@qq.com <1065277371@qq.com>
2017-06-03 11:37:15 +0800

    发发发

---
+++ src/components/app.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12"></div>
+
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <add :add="add"></add>
+      <list :comments="comments" :remove="remove"></list>
+    </div>
+  </div>
+</template>
+<script>
+  import add from './add'
+  import list from './list'
+
+  export default {
+    data () {
+      return {
+        comments: [
+          {username: '浅浅', content: '好美的风景'},
+          {username: '落落', content: '好喝的水'}
+        ]
+      }
+    },
+
+    methods: {
+      add (comment) {
+        this.comments.unshift(comment)
+      },
+      remove (comment) {
+        this.comments.$remove(comment)
+      }
+    },
+
+    components: {
+      add, list
+    }
+  }
+
+</script>
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import app from './components/app'
+
+/* eslint-disable no-new */
+
+new Vue({
+  el: 'body',
+  components: { app }
+})
